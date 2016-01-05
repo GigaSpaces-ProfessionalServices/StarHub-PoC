79f20a46135db34149fb9fce016327eba644a04f
alon-strik <alons@gigaspaces.com>
2016-01-05 13:13:25 +0200

    vyatta

--- plugins/cloudify-vyatta-plugin/vyatta_plugin/tasks.py
+++ plugins/cloudify-vyatta-plugin/vyatta_plugin/tasks.py
@@ -1,16 +1,126 @@
 from cloudify.decorators import operation
 from cloudify.state import ctx_parameters as inputs
+import re
+import urllib
+import requests
+
+requests.packages.urllib3.disable_warnings()  # Surpress "InsecureRequestWarning" warning
 
 
-forti_username = 'vyatta'
-forti_password = 'vyatta'
-portMask = '255.255.255.0'
+class VyattaControl(object):
+    """
+    Provides methods to show and modify Vyatta status and configurations.
+    """
+
+    def __init__(self, urlBase, user, passwd, ctx):
+
+        self.urlBase = urlBase
+        self.urlConfBase = urlBase + 'rest/conf'
+        self.urlOpBase = urlBase + 'rest/op'
+        self.user = user
+        self.passwd = passwd
+
+    def getOpId(self, urlOpId, ctx):
+        """
+        Get the operation id, which is substring of the Location header in HTTP response.
+        """
+
+        rop = requests.post(urlOpId, auth=(self.user, self.passwd), verify=False)  # Request to get operation id
+        return rop.headers['Location'].split('/')[2]  # Get Location header
+
+    def getConfId(self, ctx):
+        """
+        Get the configuration id, which is substring of the Location header in HTTP response.
+        """
+
+        rconf = requests.post(self.urlConfBase, auth=(self.user, self.passwd), verify=False)
+        return rconf.headers['Location'].split('/')[2]
+
+    def deleteConfId(self, confId, ctx):
+        """
+        Delete existing Vyatta configuration session
+        """
+
+        urlConfDelete = self.urlConfBase + '/' + confId
+        rdel = requests.delete(urlConfDelete, auth=(self.user, self.passwd), verify=False)
+        return rdel.status_code
+
+    def commandOperational(self, opCommands, ctx):
+        """
+        Call Vyatta operational mode commands from opCommands list.
+        """
+        for line in opCommands:
+            urlOpCommand = self.urlOpBase + '/' + '/'.join(line.split(None))
+            ropResult = requests.get(self.urlOpBase + '/' + self.getOpId(urlOpCommand),
+                                     auth=(self.user, self.passwd),
+                                     verify=False)  # Request to get the results
+
+            ctx.logger.info('$ : {0}'.format(line))
+            ctx.logger.info('{0}'.format(ropResult.text))
+
+    def createEncodedUrl(self, confId, string, ctx):
+        """
+        URLencode every configuration words and form proper URL for REST API requests.
+        :param confId: Configuration session ID
+        :param string: One line Vyatta configuration commands and parameters
+        :return: Encoded URL for Vyatta REST API
+        """
+
+        encodedWord = []
+        for word in string.split():
+            encodedWord.append(urllib.quote(word, safe=""))  # Encode each words, then make a list of words
+
+        encodedUrl = self.urlConfBase + '/' + confId + '/' + '/'.join(' '.join(encodedWord).split(None))
+        return encodedUrl
+
+    def editConfig(self, config, ctx):
+        """
+        Read configurations from a LIST and send requests to Vyatta via REST API,
+        then actually modify Vyatta configuration and commit configuration changes.
+        """
+
+        # Set configurations
+        confId = self.getConfId()  # Get configuration ID
+
+        for line in config:
+            if not (re.compile("^#").match(line)
+                    or re.compile("^$").match(line)):  # Skip line matches with "^#" or "^$"
+                urlConfPut = self.createEncodedUrl(confId, line)
+                rconf = requests.put(urlConfPut,
+                                     auth=(self.user, self.passwd),
+                                     verify=False)  # Request for configuration commands
+
+                ctx.logger.info('{0}  :  {1}'.format(urlConfPut, rconf.status_code))
+
+        self.commitConfig(confId)  # Commit configurations
+        self.saveConfig(confId)  # Save configurations
+        return self.deleteConfId(confId)  # Delete conf-id and return HTTP status code
+
+    def commitConfig(self, confId, ctx):
+        """
+        Commit configuration changes
+        """
+
+        urlConfCommit = self.urlConfBase + '/' + confId + '/commit'
+        rconf = requests.post(urlConfCommit, auth=(self.user, self.passwd), verify=False)  # Request for commit
+        ctx.logger.info('{0}  :  {1}'.format(urlConfCommit, rconf.status_code))
+        return rconf.status_code
+
+    def saveConfig(self, confId, ctx):
+        """
+         Save changes
+        """
+        urlConfSave = self.urlConfBase + '/' + confId + '/save'
+        rconf = requests.post(urlConfSave, auth=(self.user, self.passwd), verify=False)  # Request for save
+        ctx.logger.info('{0}  :  {1}'.format(urlConfSave, rconf.status_code))
+        return rconf.status_code
 
 
 @operation
 def port_config(ctx, **kwargs):
     ctx.logger.info('Start port config task....')
-    port_id = 2  # port 1 reserved for admin
+    command = []
+    port_idx = 4
 
     vyatta_host_ip = get_host_ip(ctx)
     ctx.logger.info('vyatta_host_ip: {0}'.format(vyatta_host_ip))
@@ -19,105 +129,28 @@ def port_config(ctx, **kwargs):
         ctx.logger.info('RELATIONSHIP type : {0}'.format(relationship.type))
 
         if 'connected_to' in relationship.type:
-            port_alias = relationship.target.node.name
-            ctx.logger.info('RELATIONSHIP target node name: {0}'.format(port_alias))
-
             target_ip = relationship.target.instance.runtime_properties['fixed_ip_address']
             ctx.logger.info('TARGET IP target_ip : {0}'.format(target_ip))
+            cmd = 'set interfaces dataplane dp0s' + port_idx + ' address ' + target_ip + '/24'
+            command.append(cmd)
+            port_idx = +1
 
-            set_port(ctx, vyatta_host_ip, target_ip, port_id, port_alias)
-            port_id += 1
-
-
-def set_port(ctx, vyatta_host_ip, target_ip, port_id, port_alias):
-    ctx.logger.info('Configure fw port ....')
-
-    command = \
-        'config system interface\n' \
-        '   edit port%s\n' \
-        '       set mode static\n' \
-        '       set allowaccess ping\n' \
-        '       set alias %s\n' \
-        '       set ip %s  %s\n' \
-        '   next\n' \
-        'end' % (port_id, port_alias, target_ip, portMask)
-
-    exec_command(ctx, command, vyatta_host_ip)
-
-
-@operation
-def policy_config(ctx, **kwargs):
-    ctx.logger.info('Start policy task....')
-
-    vyatta_host_ip = get_host_ip(ctx)
-    ctx.logger.info('vyatta_host_ip: {0}'.format(vyatta_host_ip))
-
-    set_policy(ctx, vyatta_host_ip)
-
-
-def set_policy(ctx, vyatta_host_ip):
-
-    command = \
-        'config firewall policy\n' \
-        '  edit 1\n' \
-        '    set srcintf \"any\"\n' \
-        '    set dstintf \"any\"\n' \
-        '    set srcaddr \"all\"\n' \
-        '    set dstaddr \"all\"\n' \
-        '    set action accept\n' \
-        '    set schedule \"always\"\n' \
-        '    set service \"ALL\"\n' \
-        '  next\n' \
-        'end'
-
-    exec_command(ctx, command, vyatta_host_ip)
-
-@operation
-def route_config(ctx, **kwargs):
-    ctx.logger.info('Start route task....')
-
-    vyatta_host_ip = get_host_ip(ctx)
-    ctx.logger.info('vyatta_host_ip: {0}'.format(vyatta_host_ip))
-
-    target_ip = '0.0.0.0'
-
-# if relationship is attached get the ip
-
-    for relationship in ctx.instance.relationships:
-        if 'connected_to' in relationship.type:
-            target_ip = relationship.target.instance.runtime_properties['fixed_ip_address']
-
-    gateway = inputs['gateway']
-    port_id = 3
-    portMask = '0.0.0.0'
-
-    set_route(ctx, vyatta_host_ip, gateway, target_ip, portMask, port_id)
-
-
-def set_route(ctx, vyatta_host_ip, gateway, target_ip, portMask, port_id):
-
-    command = \
-        'config router static\n' \
-        '  edit 1\n' \
-        '    set dst %s %s\n' \
-        '    set gateway %s\n' \
-        '    set device port%s\n' \
-        '  next\n' \
-        'end' % (target_ip, portMask, gateway, port_id)
-
-    exec_command(ctx, command, vyatta_host_ip)
+        exec_command(ctx, command, vyatta_host_ip)
 
 
 def exec_command(ctx, command, vyatta_host_ip):
 
     ctx.logger.info('Open connection to host {0} '.format(vyatta_host_ip))
-    conn = FortiOS(vyatta_host_ip, username=forti_username, password=forti_password)
-    conn.open()
 
-    ctx.logger.info("Execute Command >> \n {0}".format(command))
+    vyatta_username = 'vyatta'
+    vyatta_password = 'vyatta'
+    urlBase = 'https://' + vyatta_host_ip + '/'
 
-    conn.execute_command(command)
-    conn.close()
+    vy = VyattaControl(ctx, urlBase, vyatta_username, vyatta_password)
+
+    vy.editConfig(command)
+
+    #vy.commandOperationalList(['show interfaces'])
 
 
 def get_host_ip(ctx):
