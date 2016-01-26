34bd7558dbe39fdc42ad59a2ca1c808cf90ce846
alon-strik <alons@gigaspaces.com>
2016-01-26 19:53:41 +0200

    notes

--- FW-Fortinet-blueprint/plugins/cloudify-fortinet-plugin/setup.py
+++ FW-Fortinet-blueprint/plugins/cloudify-fortinet-plugin/setup.py
@@ -35,8 +35,8 @@ setup(
     zip_safe=False,
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
-        "cloudify-plugins-common>=3.3",
-        "pyFG"
+        "cloudify-plugins-common>=3.3"
+#        "pyFG"
     ],
     test_requires=[
         "cloudify-dsl-parser>=3.3"
